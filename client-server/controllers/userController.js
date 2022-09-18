73a012887d813da0db6262b6a2d75bc0a62cbe1e
Jun Li <junchenli423@gmail.com>
2022-09-17 23:25:06 -0400

    asdf

--- client-server/controllers/userController.js
+++ client-server/controllers/userController.js
@@ -1,13 +1,37 @@
 const users = require('../models/general.model')[2]
 const pharmacies = require('../models/general.model')[0]
 
-exports.placeOrder = function(req,res, next){
-    const pharmacy = new pharmacies({
-        name: Math.floor(Math.random() * Math.random() * 100).toString(10)
-    }).save(function(err){
-        if(err){
-            console.log(err)
+exports.placeOrder = async function(req,res, next){
+    /* POSTMAN REQ
+        {
+            "name": "124",
+            "request": {
+                "supplier": "69",
+                "landing-area": {"long": 63, "lat": 23},
+                "home": {"long": 63, "lat": "23"},
+                "pill": "tylenol",
+                "shop": "shop"
+            }
         }
+     */
+    pharmacies.find({name: req.body.request.supplier}, function(err,pharmacies){
+        let pending = [...pharmacies[0].pending]
+        pending.push(req.body.request)
+        pharmacies[0].update({"pending": pending}, function(err){
+            if(err){
+                res.json(err)
+            }
+        })
     })
+    users.find({firstName: req.body.name}, function(err, users){
+        let active = [...users[0].active]
+        active.push(req.body.request)
+        users[0].update({"active": active}, function(err){
+            if(err){
+                res.json(err)
+            }
+        })
+    })
+
     res.json('hi')
 }
