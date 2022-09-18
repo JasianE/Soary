f4f419f1ffa23b7574060cbe582c1b4760f426e5
Bilal Shaikh <bilalwaaiz@gmail.com>
2022-09-17 22:43:17 -0400

    pushed movement endpoint

--- server/application.py
+++ server/application.py
@@ -1,46 +1,73 @@
 import flask
-import pymongo
+from djitellopy import Tello
+import time
+import math
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-client = pymongo.MongoClient(
-    "mongodb+srv://esambald:Yj39ual8lXxmwUd4@cluster0.swylr9h.mongodb.net/?retryWrites=true&w=majority"
-)
-db = client.test
+@app.route("/api/v1/kill", methods=["POST"])
+def kill():
+    tello = Tello()
+    time.sleep(1.5)
+    tello.connect()
+    time.sleep(1)
+    tello.land()
 
 
-# write a post method to create a new "drone supplier user" with fields email, password, first name, last name, drone model
-@app.route("/api/v1/users", methods=["POST"])
-def create_user():
+@app.route("/api/v1/move", methods=["POST"])
+def move():
+    '''
+    //real
+    get pharmacy coordinates from server 
+    flask server will serve flight info to drone 
+    we are happy :D
+
+    '''
+    ##recieve data from server.js
+    # request_data = flask.request.get_json()
+    # lat = request_data['latitude']
+    # long = request_data['longitude']
+    # #get long lat data form database
+
     request_data = flask.request.get_json()
-    email = request_data["email"]
-    password = request_data["password"]
-    first_name = request_data["first_name"]
-    last_name = request_data["last_name"]
-    drone_model = request_data["drone_model"]
-    # create a new user
-    supplier = db.supplier  # collection name
-    user = {
-        "email": email,
-        "password": password,
-        "first_name": first_name,
-        "last_name": last_name,
-        "drone_model": drone_model,
-        "home": "",  # gps coordinates
-    }
-    supplier.insert_one(
-        {
-            "email": email,
-            "password": password,
-            "first_name": first_name,
-            "last_name": last_name,
-            "drone_model": drone_model,
-        }
-    )
-    return flask.jsonify({"message": "user created successfully", "user": user}), 201
+    lat = request_data['latitiude']
+    long = request_data['longitude']
+    destination = request_data['destination']
+    coords = [lat, long]
+
+    #initialize and takeoff drone
+    tello = Tello()
+    time.sleep(3)
+    tello.connect()
+    time.sleep(3)
+    tello.takeoff()
+    supplier_loc = [0,0]
+
+    #form a right triangle, find opp and adj for trig 
+    opp = int(coords[0]) - int(supplier_loc[0])
+    adj = int(coords[1]) - int(supplier_loc[1])
+
+
+    #find takeoff angle
+    takeoff_angle = math.floor(math.degrees(math.atan(int(opp) / int(adj))))
+
+    #turn and move to position , then land
+    time.sleep(3)
+    tello.rotate_counter_clockwise(takeoff_angle)
+    time.sleep(3)
+    dist = math.floor(math.sqrt(opp ** 2 + adj **2))
+    tello.move_forward(dist)
+    print(takeoff_angle)
+    print(math.sqrt(math.floor(opp ** 2 + adj **2)))
+    time.sleep(3)
+    tello.land()
+    tello.emergency()
+
+    return flask.jsonify({"message": "drone succesfully deployed to", "destination" : destination  }, 201)
 
 
 # run the application
 app.run()
+#pushed
